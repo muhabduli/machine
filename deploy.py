96613108a9b879586aace6427014756d473ea0c1
muhabduli <125576283+muhabduli@users.noreply.github.com>
2023-04-05 15:44:33 +0100

    jhgj

--- deploy.py
+++ deploy.py
@@ -1,45 +1,25 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "0b5084f7",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import pandas as pd\n",
-    "from sklearn.preprocessing import StandardScaler, OrdinalEncoder, OneHotEncoder\n",
-    "\n",
-    "\n",
-    "import pickle\n",
-    "import streamlit as st\n",
-    "from streamlit_option_menu import option_menu\n",
-    "\n",
-    "\n",
-    "# loading the saved models\n",
-    "model = pickle.load(open('C:/Users/Al Amin/Downloads/NITDA_AI_Class-main/NITDA_AI_Class-main/ML_Compete/Classification/big_mart_model.pkl', 'rb'))"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.7"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, OrdinalEncoder, OneHotEncoder
+import pickle
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+
+# loading the saved models
+model = pickle.load(open('big_mart_model.pkl', 'rb'))
+# sidebar for navigation
+with st.sidebar:
+    
+    selected = option_menu('Financial Inclusion', #Title of the OptionMenu
+                          
+                          ['Financial Inclusion','Big Mart Sales Prediction'], #You can add more options to the sidebar
+                          icons=['shop', 'cash'], #BootStrap Icons - Add more depending on the number of sidebar options you have.
+                          default_index=0) #Default side bar selection
+
+    
+# bank accout Prediction Page
+if (selected == Financial Inclusion'):
+    
+    # page title
+    st.title('Financial Inclusion')
+    
