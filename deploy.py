da9219b0537715b17aae5eda9b0c2a2ac74ee5d0
muhabduli <125576283+muhabduli@users.noreply.github.com>
2023-04-05 15:46:42 +0100

    jh

--- deploy.py
+++ deploy.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from sklearn.preprocessing import StandardScaler, OrdinalEncoder, OneHotEncoder
 import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
